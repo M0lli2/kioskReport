b830213ebad21e3444fb998edfe961a047a33f50
M0lli2 <bsg8247@gmail.com>
2025-03-07 18:35:11 +0900

    25-03-07 Lv1 start

---
+++ src/main/java/org/example/lv1/ChickenMenu.java
@@ -0,0 +1,13 @@
+package org.example.lv1;
+
+import java.util.Scanner;
+
+public class ChickenMenu {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+
+
+
+    }
+}
